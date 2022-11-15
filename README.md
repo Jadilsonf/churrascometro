38d40d8337e917fe9f0aa17a5de3d6b5b1997a1a
jadilsonf <jadilsonfilho171292@gmail.com>
2022-11-15 14:42:39 -0300

    criado o inputs no html

---
+++ README.md
@@ -0,0 +1,4 @@
+## Carne - 400gr por pessoa + de 6 horas - 650
+## Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
+## Refrigerante/agua - 1000 ml por pessoa + 6 horas
+## Crianças vaem por 0.5
--- script.js
+++
@@ -1,5 +0,0 @@
-/** Carne - 400gr por pessoa + de 6 horas - 650
- * Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
- * Refrigerante/agua - 1000 ml por pessoa + 6 horas
- * crianças vaem por 0.5
- */
